1316eb59f3e8b357d9fae7ef925488d0c109ecad
geison-lira <geisonvlf.dev@gmail.com>
2026-04-16 23:33:22 -0300

    style: reformat line breaks in problem 1

--- src/problem1.py
+++ src/problem1.py
@@ -5,11 +5,8 @@ from typing import List
 # Actual solution below
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-
         diffs = {}
-
         for idx, num in enumerate(nums):
             if num in diffs.keys():
                 return [diffs[num], idx]
-
             diffs[target - num] = idx

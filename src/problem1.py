6384220452ee00c5f4b2d2ce251ddaab5e268661
geison-lira <geisonvlf.dev@gmail.com>
2026-05-13 23:57:04 -0300

    feat: add solution to problem 1

---
+++ src/problem1.py
@@ -0,0 +1,12 @@
+# Imports (just for testing, DO NOT COPY THESE)
+from typing import List
+
+
+# Actual solution below
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        diffs = {}
+        for idx, num in enumerate(nums):
+            if num in diffs:
+                return [diffs[num], idx]
+            diffs[target - num] = idx
